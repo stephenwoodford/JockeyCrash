39dbdc53346201d6686233ac4d092e6043893fbb
Stephen Woodford <stephen.woodford@aisle50.com>
2014-09-23 10:26:42 -0500

    Show crash using minimum implementation of extending TiUIWebView (https://github.com/stephenwoodford/NappJockey/tree/minimum)

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,3 @@
-$.webView.url = "https://www.google.com"; // This line causes the crash in JockeyCrash Log 1
+//$.webView.url = "https://www.google.com"; // This line causes the crash in JockeyCrash Log 1
 
 $.index.open();
